136d150c53f3ea6cf237dd5a4b971c7324f039fc
tbreijak <ab6131@wayne.edu>
2016-02-19 10:10:37 -0500

    trying to make a ball
    no balls

--- sketch.js
+++ sketch.js
@@ -1,3 +1,23 @@
+var MovableCircle = function (x, y) {
+  this.position = new p5.vector(x, y);
+
+};
+  MovableCircle.prototype = {
+
+    radius: 30,
+    initialize: function(){};
+
+    update: function(){};
+
+    display: function(){};
+
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(this.position.x, this.position.y, this.radius / 2, this.radius / 2);
+  };
+
+    createCanvas(400, 400);
+    var myMovingBall = new MovableCircle(width / 2, height / 2);
 setup = function() {
 
 };
