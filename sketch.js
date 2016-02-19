011eafcc18fcc8aa683f76af3a52108a90b4d81e
courtneyhale <courtneyhale44@gmail.com>
2016-02-18 21:58:23 -0500

    Added circle with W move up

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+var MovableCircle = function(x, y){
+  this.position = new p5.Vector(x,y);
+};
+
+MovableCircle.prototype = {
+
+  radius: 70,
+
+  initialize: function () {
+    this.upListener = new KeyListener('W', this.moveUp, this);
+  },
+
+  update: function () {
+    this.upListener.listen();
+  },
+
+  display: function () {
+    noStroke();
+    fill(255,0,0);
+    ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
+  },
+
+
+  moveUp: function(){
+    this.position.y -= 1;
+  },
+};
+
+var myMovingBall;
+
 setup = function() {
+  createCanvas(400, 400);
+  myMovingBall = new MovableCircle(width / 2, height / 2);
+  myMovingBall.initialize();
 
 };
 
 draw = function() {
+  background(255);
+
+  myMovingBall.update();
+  myMovingBall.display();
 
 };
