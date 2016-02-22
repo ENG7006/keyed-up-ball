bd5c418a08cca9fca3f7029dfef796ba6c8a816b
NinjAdina <AdinaShuttari@Adinas-MacBook-Pro.local>
2016-02-21 19:03:14 -0500

    Ball moves W, A, S, D but Off Canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,54 @@
-setup = function() {
+var MovableCircle = function (x,y) {
+  this.position = new p5.Vector (x,y);
 
 };
 
-draw = function() {
+MovableCircle.prototype = {
+  radius: 10,
 
+  initialize: function () {
+    this.upListener = new KeyListener('W', this.moveUp, this);
+    this.downListener = new KeyListener('S', this.moveDown, this);
+    this.rightListener = new KeyListener('D', this.moveRight, this);
+    this.leftListener = new KeyListener('A', this.moveLeft, this);
+  },
+
+  update: function () {
+    this.upListener.listen();
+    this.downListener.listen();
+    this.rightListener.listen();
+    this.leftListener.listen();
+  },
+
+  display: function () {
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
+  },
+  moveUp: function () {
+    this.position.y -= 1;
+  },
+  moveDown: function () {
+    this.position.y += 1;
+  },
+  moveRight: function () {
+    this.position.x += 1;
+  },
+  moveLeft: function () {
+    this.position.x -= 1;
+  },
+};
+var myMovingBall;
+
+setup = function() {
+  createCanvas (600, 600);
+  myMovingBall = new MovableCircle(width/2, height/2);
+  myMovingBall.initialize();
+};
+
+draw = function() {
+  background(200);
+
+  myMovingBall.update ();
+  myMovingBall.display ();
 };
